60e14f1d2eb0c8e53d90ea6a4bfc53675f2d8da6
DmitryChura <109786078+DmitryChura@users.noreply.github.com>
2022-07-28 14:31:48 +0300

    Update assignment_1.py

--- Extracting data from a text file/assignment_1.py
+++ Extracting data from a text file/assignment_1.py
@@ -1,18 +1,27 @@
-Write a program to read text file (file_1.txt) and figure out who has sent the greatest number of mail messages.
+...
+Write a program to read text file (mbox-short.txt) and figure out who has sent the greatest number of mail messages.
+The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. 
+The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. 
+After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+...
 
 name = input("Enter file:")
 handle = open(name)
 counts = dict()
+
 for line in handle:
     line = line.rstrip()
     if line.startswith('From '):
         words = line.split()
         pieces = words[1]
         counts[pieces] = counts.get(pieces, 0) + 1
+
 largest = -1
 bigword = None
+
 for pieces,value in counts.items():
     if value > largest:
         largest = value
         bigword = pieces
+
 print(bigword, largest)
